3aa5a9ccd029f0a0f23cc31aa2e93d620b739d0d
AimarGende <aimargende@gmail.com>
2024-01-26 14:23:56 +0100

    Empezado larqaavel de captura de bases

--- DWC/Pruebas/APP/scripts/fetch.js
+++ DWC/Pruebas/APP/scripts/fetch.js
@@ -24,10 +24,67 @@ function RecogerLocalizaciones(idsZonas) {
                 // console.log(data["forecastText"])
                 for (let location of data) {
                     if (location["regionZoneLocationId"] == "barakaldo" || location["regionZoneLocationId"] == "bilbao" || location["regionZoneLocationId"] == "irun" || location["regionZoneLocationId"] == "zarautz" || location["regionZoneLocationId"] == "errenteria" || location["regionZoneLocationId"] == "donostia") {
+                        console.log(location)
                         localizaciones.push(location["regionZoneLocationId"])
                     }
                 }
             })
     }
+
     return localizaciones
+}
+
+function RecogerDatos(codigos) {
+    let ciudades = new Array()
+    for (let provincia in codigos) {
+        for (let carac in codigos[provincia]) {
+            for (let ciudadCOD in codigos[provincia][carac]) {
+                let fetchURL = `https://www.el-tiempo.net/api/json/v2/provincias/${codigos[provincia].ID}/municipios/${codigos[provincia][carac][ciudadCOD]}`
+                fetch(fetchURL)
+                    .then(response => {
+                        if (!response.ok) {
+                            throw new Error("La solicitud no se pudo completar correctamente.");
+                        }
+                        return response.json();
+                    })
+                    .then(data => {
+
+                        let ciudad = {
+                            nombre: data["municipio"]["NOMBRE"],
+                            latitud: data["municipio"]["LATITUD_ETRS89_REGCAN95"],
+                            longitud: data["municipio"]["LONGITUD_ETRS89_REGCAN95"],
+                            temperatura: ((data["temperatura_actual"] == "") ? 0 : data["temperatura_actual"]),
+                            humedad: ((data["humedad"] == "") ? 0 : data["humedad"]),
+                            lluvia: ((data["lluvia"] == "") ? 0 : data["lluvia"]),
+                            viento: ((data["viento"] == "") ? 0 : data["viento"]),
+                            precipitacion: ((data["precipitacion"] == "") ? 0 : data["precipitacion"])
+                        }
+                        if (codigos[provincia][carac][ciudadCOD] == 20069) {
+                            console.log(ciudad.nombre)
+                            ciudad.nombre = "Donostia/San Sebastian"
+                        }
+                        ciudades.push(ciudad)
+                    })
+            }
+        }
+    }
+    return ciudades
+
+}
+function InsertarDatos(ciudades) {
+    for (let ciudad of ciudades) {
+        fetch("http://185.60.40.210/dwc/Aimar/Proyecto/insertarDatosBase.php?ciudad=" + JSON.stringify(ciudad))
+    }
+}
+
+function DatosAleatorios(ciudades) {
+    let ciudadesAleatorias = ciudades
+    ciudadesAleatorias.forEach(ciudad => {
+        ciudad["temperatura"] = parseInt(ciudad["temperatura"]) + (Math.round(Math.random()) * 2 - 1)
+        ciudad["humedad"] = parseInt(ciudad["humedad"]) + ((Math.round(Math.random()) * 2 - 1) < 0 ? 1 : (Math.round(Math.random()) * 2 - 1))
+        ciudad["lluvia"] = parseInt(ciudad["lluvia"]) + ((Math.round(Math.random()) * 2 - 1) < 0 ? 1 : (Math.round(Math.random()) * 2 - 1))
+        ciudad["viento"] = parseInt(ciudad["viento"]) + ((Math.round(Math.random()) * 2 - 1) < 0 ? 1 : (Math.round(Math.random()) * 2 - 1))
+        ciudad["precipitacion"] = parseInt(ciudad["precipitacion"]) + ((Math.round(Math.random()) * 2 - 1) < 0 ? 1 : (Math.round(Math.random()) * 2 - 1))
+    });
+    return ciudadesAleatorias
 }
--- DWC/Pruebas/APP/scripts/variables.js
+++ DWC/Pruebas/APP/scripts/variables.js
@@ -4,6 +4,26 @@ var lugares = [
     { "nombre": "Renteria", "latitud": 43.3119, "longitud": -1.8985, "temperatura": "5ºC", "humedad": "39%", "viento": "32 m/s", "nubes": "0%", "lluvia": "0mm" },
     { "nombre": "Bilbao", "latitud": 43.266134, "longitud": -2.936054, "temperatura": "30ºC", "humedad": "78%", "viento": "42 m/s", "nubes": "200%", "lluvia": "10mm" }
 ]
+const codigos = {
+    Bizkaia: {
+        ID: 48,
+        Ciudades: {
+            Bilbao: 48020,
+            Barakaldo: 48013
+        }
+
+    },
+    Gipuzkoa: {
+        ID: 20,
+        Ciudades: {
+            Zarautz: 20079,
+            Irun: 20045,
+            Errenteria: 20067,
+            Donosti: 20069
+        }
+    },
+}
 const contenedor = document.getElementById("contenedor")
 let graficos = new Array()
 let cont = 0
+let ciudades = new Object()
