a730c98c6091138751033e1c56245cfc6a0e526c
AimarGende <aimargende@gmail.com>
2024-01-26 10:29:03 +0100

    montado todo en el apache

--- DAW/APP/scripts/APILogin.js
+++ DAW/APP/scripts/APILogin.js
@@ -1,52 +1,8 @@
-function logout() {
-    let config = {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-    }
-    fetch("http://10.10.17.121:8086/api/logout", config)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error("La solicitud no se pudo completar correctamente.");
-            }
-            return response.json();
-        })
-        .then(data => {
-            console.log(data)
-        })
-}
 
 
-function register() {
-    let data = {
-        name: document.getElementById("usuario").value,
-        email: document.getElementById("correo").value,
-        password: document.getElementById("contrasenaReg").value,
-        c_password: document.getElementById("confcontrasena").value
-    }
-    let config = {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    }
 
-
-    fetch("http://10.10.17.121:8086/api/register", config)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error("La solicitud no se pudo completar correctamente.");
-            }
-            return response.json();
-        })
-        .then(data => {
-            console.log(data)
-        })
-}
-
-function login() {
+function login(event) {
+    event.preventDefault()
     let data = {
         email: document.getElementById("email").value,
         password: document.getElementById("contrasena").value,
@@ -66,6 +22,58 @@ function login() {
             return response.json();
         })
         .then(data => {
-            console.log(data)
+            localStorage.setItem("token",data["data"]["token"])
+            window.location.assign("APP.html")
+        })
+        .catch(error => {
+            console.error('API error:', error);
+        })
+}
+
+
+function logout() {
+    let config = {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${localStorage.getItem("token")}`
+        },
+    }
+    fetch("http://10.10.17.121:8086/api/logout", config)
+    localStorage.removeItem("token")
+    window.location.assign("index.html")
+}
+
+
+function register(event) {
+    event.preventDefault()
+    let data = {
+        name: document.getElementById("usuario").value,
+        email: document.getElementById("correo").value,
+        password: document.getElementById("contrasenaReg").value,
+        c_password: document.getElementById("confcontrasena").value
+    }
+    let config = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    }
+    console.log(config)
+
+    fetch("http://10.10.17.121:8086/api/register", config)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("La solicitud no se pudo completar correctamente.");
+            }
+            return response.json();
+        })
+        .then(data => {
+            localStorage.setItem("token",data["data"]["token"])
+            window.location.assign("APP.html")
+        })
+        .catch(error => {
+            console.error('API error:', error);
         })
 }
