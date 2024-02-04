6737df844007ea13c2c0fdbd3c5a26f8efc4d354
AimarGende <aimargende@gmail.com>
2024-02-04 13:59:12 +0100

    Adapado para contenedores

--- DWC/Pruebas/APP/scripts/variables.js
+++ DWC/Pruebas/APP/scripts/variables.js
@@ -21,13 +21,13 @@ let fechaMananaSeparada = fechaManana.toISOString().split('T')[0].split('-')
 const contenedor = document.getElementById("contenedor")
 let graficos = new Array()
 let cont = 0
-// let lugares = new Array()
-let lugares = [
-    { "nombre": "Irun", "latitud": 43.3390, "longitud": -1.7896, "temperatura": "12", "humedad": "23", "viento": "22", "precipitacion": "100", "lluvia": "5000" },
-    { "nombre": "Donostia", "latitud": 43.3183, "longitud": -1.9812, "temperatura": "21", "humedad": "53", "viento": "12", "precipitacion": "43", "lluvia": "300" },
-    { "nombre": "Errenteria", "latitud": 43.3119, "longitud": -1.8985, "temperatura": "5", "humedad": "39", "viento": "32", "precipitacion": "0", "lluvia": "0" },
-    { "nombre": "Bilbao", "latitud": 43.266134, "longitud": -2.936054, "temperatura": "30", "humedad": "78", "viento": "42", "precipitacion": "200", "lluvia": "10" }
-]
+let lugares = new Array()
+// let lugares = [
+//     { "nombre": "Irun", "latitud": 43.3390, "longitud": -1.7896, "temperatura": "12", "humedad": "23", "viento": "22", "precipitacion": "100", "lluvia": "5000" },
+//     { "nombre": "Donostia", "latitud": 43.3183, "longitud": -1.9812, "temperatura": "21", "humedad": "53", "viento": "12", "precipitacion": "43", "lluvia": "300" },
+//     { "nombre": "Errenteria", "latitud": 43.3119, "longitud": -1.8985, "temperatura": "5", "humedad": "39", "viento": "32", "precipitacion": "0", "lluvia": "0" },
+//     { "nombre": "Bilbao", "latitud": 43.266134, "longitud": -2.936054, "temperatura": "30", "humedad": "78", "viento": "42", "precipitacion": "200", "lluvia": "10" }
+// ]
 
 
 
